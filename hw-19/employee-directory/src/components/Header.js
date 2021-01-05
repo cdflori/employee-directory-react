02d56da1ef1ed84a5445ed958bf1f532918940a0
Christina Flores <cdflori1@gmail.com>
2021-01-05 15:13:04 -0700

    added in UserData.js file that handles employee sorting and API call to.

---
+++ hw-19/employee-directory/src/components/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+// import '../styles/Header.css';
+
+class Header extends Component {
+    render () {
+        return (
+            <div className = 'header'>
+                <h1>Employees</h1>
+                <p>Search through employees using the search bar.</p>
+
+            </div>
+        )
+    }
+}
+
+export default Header 
